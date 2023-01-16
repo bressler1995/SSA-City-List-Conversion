c2bbd3dafa6ff42b9888fb9a53f05f52348b2fc8
carlofabyss <carlofabyss@hotmail.com>
2023-01-15 22:10:07 -0800

    Tool should work now, should help with debugging the bulk endpoints

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
+<?php
+  $citylist = '';
+  $citypresent = false;
+  $inputreload_output = '';
+  $converted_output = '';
+
+  if(empty($_GET['citylist']) == true || isset($_GET['citylist']) == false) {
+    $citypresent = false;
+  } else {
+    $citypresent = true;
+    $citylist = $_GET['citylist'];
+  }
+
+  if($citypresent == true) {
+    $text = trim($citylist);
+    $text = explode ("\n", $text);
+
+    if(count($text) > 0) {
+
+      for ($x = 0; $x < count($text); $x++) {
+        $islast = false;
+        $current_text = trim($text[$x]);
+  
+        if($x + 1 == count($text)) {
+          $islast = true;
+        }
+  
+        $inputreload_output .= $current_text . "\n";
+  
+        if($islast == true) {
+          $converted_output .= $current_text;
+        } else if($islast == false) {
+          $str_withcolon = $current_text . ':';
+          $converted_output .= $str_withcolon;
+        }
+      }
+
+    }
+
+  }
+
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,10 +49,16 @@
     <link rel="stylesheet" href="index.css">
   </head>
   <body>
-    <form class="eccent_form">
+    <div class="eccent_appcontainer">
+    <form class="eccent_form" enctype="application/x-www-form-urlencoded">
         <h2>SSA City List Conversion Tool</h2>
-        <div class="eccent_form_inputContainer"><textarea name=""></textarea></div>
-        <div class="eccent_form_inputContainer"><input type="submit" value="Submit"></div>
+        <div class="eccent_form_inputContainer"><textarea class="citylist" name="citylist" placeholder="List Cities like: City, State"><?php echo $inputreload_output ?></textarea></div>
+        <div class="eccent_form_inputContainer"><input class="submitopt" type="submit" value="Submit"></div>
+        <div class="eccent_form_inputContainer">
+          <h4>Output:</h4>
+          <div><?php echo $converted_output ?></div>
+        </div>
     </form>
+    </div>
   </body>
 </html>
