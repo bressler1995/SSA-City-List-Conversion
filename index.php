1eea3db63c3d9fb5b39db800527f739c866e9116
carlofabyss <carlofabyss@hotmail.com>
2023-01-15 20:48:44 -0800

    Front end of tool

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SSA City List Conversion Tool</title>
+    <link rel="stylesheet" href="index.css">
+  </head>
+  <body>
+    <form class="eccent_form">
+        <h2>SSA City List Conversion Tool</h2>
+        <div class="eccent_form_inputContainer"><textarea name=""></textarea></div>
+        <div class="eccent_form_inputContainer"><input type="submit" value="Submit"></div>
+    </form>
+  </body>
+</html>
